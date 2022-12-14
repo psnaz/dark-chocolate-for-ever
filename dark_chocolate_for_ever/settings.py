f332a86469d7cb921a2a46848241e148798d3554
psnaz <psnazle@gmail.com>
2022-12-14 12:56:34 +0000

    Set up allauth

--- dark_chocolate_for_ever/settings.py
+++ dark_chocolate_for_ever/settings.py
@@ -82,6 +82,17 @@ AUTHENTICATION_BACKENDS = [
 
 SITE_ID = 1
 
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+ACCOUNT_AUTHENTICATION_METHOD = 'username_email'
+ACCOUNT_EMAIL_REQUIRED = True
+ACCOUNT_EMAIL_VERIFICATION = 'mandatory'
+ACCOUNT_SIGNUP_EMAIL_ENTER_TWICE = True
+ACCOUNT_USER_NAME_MIN_LENGTH = 4
+LOGIN_URL = '/accounts/login/'
+LOGIN_REDIRECT_URL = '/'
+
+
 WSGI_APPLICATION = 'dark_chocolate_for_ever.wsgi.application'
 
 
