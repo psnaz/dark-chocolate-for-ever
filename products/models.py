66bd8c3a0937ffe9906d69a054687cdaeea11e9c
psnaz <psnazle@gmail.com>
2023-01-26 11:23:34 +0000

    Add products app, models and fixtures

---
+++ products/models.py
@@ -0,0 +1,35 @@
+from django.db import models
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=254)
+    friendly_name = models.CharField(max_length=254, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+    def get_friendly_name(self):
+        return self.friendly_name
+
+
+class Product(models.Model):
+    category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
+    sku = models.CharField(max_length=254, null=True, blank=True)
+    name = models.CharField(max_length=254)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    rating = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
+    image_url = models.URLField(max_length=1024, null=True, blank=True)
+    image = models.ImageField(null=True, blank=True)
+    brand = models.CharField(max_length=100)
+    format = models.CharField(max_length=120)
+    flavour = models.CharField(max_length=120)
+    units = models.DecimalField(max_digits=6, decimal_places=2)
+    cocoa_mass = models.DecimalField(max_digits=6, decimal_places=2,null=True, blank=True)
+    diet_type = models.CharField(max_length=120, null=True, blank=True)
+    speciality = models.CharField(max_length=254, null=True, blank=True)
+    size = models.CharField(max_length=120)
+
+
+    def __str__(self):
+        return self.name
