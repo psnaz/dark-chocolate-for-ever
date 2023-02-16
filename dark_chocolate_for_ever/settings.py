0d37d6edf1e7f906ddf0b70b9a5f4e1a83e85c2b
psnaz <psnazle@gmail.com>
2023-02-16 17:59:22 +0000

    Amend bucket config in settings.py

--- dark_chocolate_for_ever/settings.py
+++ dark_chocolate_for_ever/settings.py
@@ -172,8 +172,8 @@ if 'USE_AWS' in os.environ:
     # Bucket Config
     AWS_STORAGE_BUCKET_NAME = 'dark-chocolate-for-ever'
     AWS_S3_REGION_NAME = 'eu-west-2'
-    AWS_ACCESS_KEY_ID = os.environget('AWS_ACCESS_KEY_ID')
-    AWS_SECRET_ACCESS_KEY = os.environget('AWS_SECRET_ACCESS_KEY')
+    AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
+    AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
     AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
 
     # Static and media files
