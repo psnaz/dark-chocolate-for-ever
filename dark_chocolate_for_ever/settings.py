f3cd71b14def9669a46ebd31d0b54d85b4a22896
psnaz <psnazle@gmail.com>
2023-02-20 11:34:12 +0000

    Add cache control

--- dark_chocolate_for_ever/settings.py
+++ dark_chocolate_for_ever/settings.py
@@ -169,6 +169,12 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 if 'USE_AWS' in os.environ:
+    # Cache control
+    AWS_S3_OBJECT_PARAMETERS = {
+        'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
+        'CacheControl': 'max-age=94608000',
+    }
+
     # Bucket Config
     AWS_STORAGE_BUCKET_NAME = 'dark-chocolate-for-ever'
     AWS_S3_REGION_NAME = 'eu-west-2'
@@ -185,7 +191,9 @@ if 'USE_AWS' in os.environ:
     # Override static and media URLs in production
     STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{STATICFILES_LOCATION}/'
     MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{MEDIAFILES_LOCATION}/'
-    
+
+
+# Stripe
 
 FREE_DELIVERY_THRESHOLD = 50
 STANDARD_DELIVERY_PERCENTAGE = 10
