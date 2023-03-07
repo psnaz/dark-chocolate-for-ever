c3f0bb0d561cbe4c5dc425abc17127908ebac041
psnaz <psnazle@gmail.com>
2023-03-07 12:42:23 +0000

    Fix checkout form styling

--- checkout/forms.py
+++ checkout/forms.py
@@ -34,6 +34,6 @@ class OrderForm(forms.ModelForm):
                 placeholder = f'{placeholders[field]} *'
             else:
                 placeholder = placeholders[field]
-                self.fields[field].widget.attrs['placeholder'] = placeholder
-                self.fields[field].widget.attrs['class'] = 'stripe-style-input'
-                self.fields[field].label = False
+            self.fields[field].widget.attrs['placeholder'] = placeholder
+            self.fields[field].widget.attrs['class'] = 'stripe-style-input'
+            self.fields[field].label = False
