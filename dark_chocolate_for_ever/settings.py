c82a20956710a73b1b2d0d1c0e5fe044f5890050
psnaz <psnazle@gmail.com>
2023-03-11 14:58:19 +0000

    Fix bug: amend emailbackend in settings

--- dark_chocolate_for_ever/settings.py
+++ dark_chocolate_for_ever/settings.py
@@ -211,11 +211,11 @@ STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
 STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET', '')
 
 if 'DEVELOPMENT' in os.environ:
-    EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackEnd'
+    EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
     DEFAULT_FROM_EMAIL = 'darkchocolateforever@example.com'
 else:
-    EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackEnd'
-    EMAIL_USE_tls = True
+    EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+    EMAIL_USE_TLS = True
     EMAIL_PORT = 587
     EMAIL_HOST = 'smtp.gmail.com'
     EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
