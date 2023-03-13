0fe8b97f2427b88a19eabaa25a2a458abd7e1ffa
psnaz <psnazle@gmail.com>
2023-03-13 15:51:22 +0000

    Register reviews admin

--- reviews/admin.py
+++ reviews/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import ProductReview
 
-# Register your models here.
+
+class ReviewsAdmin(admin.ModelAdmin):
+    readonly_fields = ('submitted', 'message',)
+
+    fields = ('full_name', 'email', 'submitted', 'message',)
+
+    list_display = ('full_name', 'email', 'submitted', 'message')
+
+    ordering = ('-submitted',)   
+
+admin.site.register(ProductReview, ReviewsAdmin)
