dc4983533a5bfe74ec6783b90181653dc92c46b7
psnaz <psnazle@gmail.com>
2023-05-21 12:02:38 +0000

    Add about and terms templates, views, urls & connect

--- home/urls.py
+++ home/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+
     path('', views.index, name='home'),
+    path('about', views.about, name='about'),
+    path('terms', views.terms, name='terms'),
 ]
--- home/views.py
+++ home/views.py
@@ -2,7 +2,17 @@ from django.shortcuts import render
 
 # Create your views here.
 
-def index(request):
+def index(request):  
     """ A view to return the index page """
 
     return render(request, 'home/index.html')
+
+def about(request):
+    """ A view to return the about page """
+
+    return render(request, 'home/about.html')
+
+def terms(request):
+    """ A view to return the terms and conditions page """
+
+    return render(request, 'home/terms.html')
