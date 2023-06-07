1cac28d09493dfe80346d0e8fb7660247843cbd8
psnaz <psnazle@gmail.com>
2023-06-07 05:55:25 +0000

    Add hampers, vouchers and subscriptions pages and connect

--- home/urls.py
+++ home/urls.py
@@ -6,4 +6,7 @@ urlpatterns = [
     path('', views.index, name='home'),
     path('about', views.about, name='about'),
     path('terms', views.terms, name='terms'),
+    path('hampers', views.hampers, name='hampers'),
+    path('vouchers', views.vouchers, name='vouchers'),
+    path('subscriptions', views.subscriptions, name='subscriptions'),
 ]
--- home/views.py
+++ home/views.py
@@ -1,18 +1,38 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from django.http import HttpResponse
 
-# Create your views here.
 
-def index(request):  
+def index(request): 
     """ A view to return the index page """
 
     return render(request, 'home/index.html')
+
 
 def about(request):
     """ A view to return the about page """
 
     return render(request, 'home/about.html')
 
+
 def terms(request):
     """ A view to return the terms and conditions page """
 
     return render(request, 'home/terms.html')
+
+
+def hampers(request):
+    """ A view to return the hampers page """
+
+    return render(request, 'home/hampers.html')
+
+
+def vouchers(request):
+    """ A view to return the vouchers page """
+
+    return render(request, 'home/vouchers.html')
+
+
+def subscriptions(request):
+    """ A view to return the subscriptions page """
+
+    return render(request, 'home/subscriptions.html')
