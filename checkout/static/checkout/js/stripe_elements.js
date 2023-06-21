1ddf4eff95e872c338fe92ff297ca72f9c530bbf
psnaz <psnazle@gmail.com>
2023-06-21 18:28:37 +0000

    Tidy up code in stripe_element.js

--- checkout/static/checkout/js/stripe_elements.js
+++ checkout/static/checkout/js/stripe_elements.js
@@ -54,7 +54,7 @@ form.addEventListener('submit', function(ev) {
         'csrfmiddlewaretoken': csrfToken,
         'client_secret': clientSecret,
         'save_info': saveInfo,
-    }
+    };
     var url = '/checkout/cache_checkout_data/';
 
     $.post(url, postData).done(function() {
@@ -108,5 +108,5 @@ form.addEventListener('submit', function(ev) {
     }).fail(function() {
         // just reload the page, the error will be in django messages
         location.reload();
-    })   
+    });   
 });
